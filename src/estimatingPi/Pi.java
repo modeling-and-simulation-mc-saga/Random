e9677d83de3689cbe46931e53dfa2677eac7464f
tada195908 <48470775+tada195908@users.noreply.github.com>
2021-03-06 11:51:33 +0900

    Add files via upload

--- src/estimatingPi/Pi.java
+++ src/estimatingPi/Pi.java
@@ -2,6 +2,7 @@ package estimatingPi;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Random;
 
 /**
  * estimating pi by simulation
@@ -12,18 +13,20 @@ public class Pi {
 
     private int in;
     private int all;
+    private Random myRandom;
 
-    public Pi() {
+    public Pi(Random myRandom) {
         in = 0;
         all = 0;
+        this.myRandom = myRandom;
     }
 
     public double addOne() {
         all++;
-        double x = Math.random();
-        double y = Math.random();
+        double x = myRandom.nextDouble();
+        double y = myRandom.nextDouble();
         double r = Math.sqrt(x * x + y * y);
-        if (r < 1.) {
+        if (r <= 1.) {
             in++;
         }
         return (double) in / all;
@@ -40,21 +43,24 @@ public class Pi {
         for (int i = 0; i < n; i++) {
             numData[i] = (int) Math.pow(2., (double) i);
         }
-
-        Pi pi = new Pi();
-        int k = 1;
+        Random myRandom = new Random(32943L);
+        Pi pi = new Pi(myRandom);
+        int k = 0;
         int t = 0;
         String filename = Pi.class.getSimpleName() + "-output.txt";
-        try (BufferedWriter out = myLib.utils.FileIO.openWriter(filename)) {
+        try ( BufferedWriter out = myLib.utils.FileIO.openWriter(filename)) {
             while (k < n) {
-                double x = pi.addOne();
-                if (t == numData[k]) {
-                    //推計したpiの値と厳密な値の差
-                    double r = Math.abs(x - Math.PI/4);
-                    myLib.utils.FileIO.writeSSV(out, t, x, r);
-                    k++;
+                while (true) {
+                    t++;
+                    double x = pi.addOne();
+                    if (t == numData[k]) {
+                        //推計したpiの値と厳密な値の差
+                        double r = Math.abs(x - Math.PI / 4);
+                        myLib.utils.FileIO.writeSSV(out, t, x, r);
+                        break;
+                    }
                 }
-                t++;
+                k++;
             }
         }
     }
